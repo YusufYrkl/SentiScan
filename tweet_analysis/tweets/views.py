945ec3e6d352aa2449bef658c86a83f4de99f066
TolgaT <tolga.topal@stud.fh-campuswien.ac.at>
2024-06-18 23:25:42 +0200

    Added Filtering and styling

--- tweet_analysis/tweets/views.py
+++ tweet_analysis/tweets/views.py
@@ -2,5 +2,23 @@ from django.shortcuts import render
 from .models import TweetAnalysis
 
 def tweet_list(request):
+    sentiment = request.GET.get('sentiment')
+    min_confidence = request.GET.get('min_confidence')
+    keyword = request.GET.get('keyword')
+
     tweets = TweetAnalysis.objects.all()
+
+    if sentiment:
+        tweets = tweets.filter(sentiment=sentiment)
+
+    if min_confidence:
+        try:
+            min_confidence = float(min_confidence)
+            tweets = tweets.filter(confidence__gte=min_confidence)
+        except ValueError:
+            pass  # Ignore invalid input
+
+    if keyword:
+        tweets = tweets.filter(tweet__icontains=keyword)
+
     return render(request, 'tweets/tweet_list.html', {'tweets': tweets})
