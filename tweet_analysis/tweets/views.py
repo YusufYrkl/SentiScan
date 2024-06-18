708d320547d93426452aa75265baed6e3861705a
Yunus Emre Kara <yunuskr40@gmail.com>
2024-06-18 23:42:33 +0200

    added data visualization of the sentiment analysis

--- tweet_analysis/tweets/views.py
+++ tweet_analysis/tweets/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.db.models import Count
 from .models import TweetAnalysis
+
 
 def tweet_list(request):
     tweets = TweetAnalysis.objects.all()
-    return render(request, 'tweets/tweet_list.html', {'tweets': tweets})
+    sentiment_counts = tweets.values('sentiment').annotate(total=Count('sentiment')).order_by('sentiment')
+    sentiment_data = {item['sentiment']: item['total'] for item in sentiment_counts}
+    return render(request, 'tweets/tweet_list.html', {'tweets': tweets, 'sentiment_data': sentiment_data})
