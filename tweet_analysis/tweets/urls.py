6bc52af58709b746204915c03eebd3b5f730cc89
Yunus Emre Kara <yunuskr40@gmail.com>
2024-06-20 01:33:51 +0200

    added graphs page and pie chart

--- tweet_analysis/tweets/urls.py
+++ tweet_analysis/tweets/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.tweet_list, name='tweet_list'),
+    path('graphs/', views.graphs, name='graphs'),
 ]
--- tweet_analysis/tweets/views.py
+++ tweet_analysis/tweets/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 from .models import TweetAnalysis
 from django.db.models import Count
 
+def get_sentiment_data():
+    tweets = TweetAnalysis.objects.all()
+    sentiment_counts = tweets.values('sentiment').annotate(total=Count('sentiment')).order_by('sentiment')
+    sentiment_data = {item['sentiment']: item['total'] for item in sentiment_counts}
+    return sentiment_data
+
 def tweet_list(request):
     sentiment = request.GET.get('sentiment')
     min_confidence = request.GET.get('min_confidence')
@@ -22,8 +28,10 @@ def tweet_list(request):
     if keyword:
         tweets = tweets.filter(tweet__icontains=keyword)
 
-    # Count the number of tweets for each sentiment
-    sentiment_counts = tweets.values('sentiment').annotate(total=Count('sentiment')).order_by('sentiment')
-    sentiment_data = {item['sentiment']: item['total'] for item in sentiment_counts}
+    sentiment_data = get_sentiment_data()
 
     return render(request, 'tweets/tweet_list.html', {'tweets': tweets, 'sentiment_data': sentiment_data})
+
+def graphs(request):
+    sentiment_data = get_sentiment_data()
+    return render(request, 'tweets/graphs.html', {'sentiment_data': sentiment_data})
