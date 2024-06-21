db185f4ede05e15455baa0a5c02cc4927dd4f9b8
Yusuf <yusuf.yuerekli@stud.fh-campuswien.ac.at>
2024-06-21 15:53:19 +0200

    Added export to csv functionality and url session managment of filtered data

--- tweet_analysis/tweets/urls.py
+++ tweet_analysis/tweets/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.tweet_list, name='tweet_list'),
+    path('export/', views.export_tweets, name='export_tweets'),
     path('graphs/', views.graphs, name='graphs'),
 ]
--- tweet_analysis/tweets/views.py
+++ tweet_analysis/tweets/views.py
@@ -1,14 +1,18 @@
+import csv
+from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import TweetAnalysis
 from django.db.models import Count
 import subprocess
 import os
 
+
 def get_sentiment_data():
     tweets = TweetAnalysis.objects.all()
     sentiment_counts = tweets.values('sentiment').annotate(total=Count('sentiment')).order_by('sentiment')
     sentiment_data = {item['sentiment']: item['total'] for item in sentiment_counts}
     return sentiment_data
+
 
 def tweet_list(request):
     sentiment = request.GET.get('sentiment')
@@ -46,7 +50,41 @@ def tweet_list(request):
             print(e)
         return redirect('tweet_list')
 
-    return render(request, 'tweets/tweet_list.html', {'tweets': tweets, 'sentiment_data': sentiment_data, 'total_tweets': total_tweets})
+    return render(request, 'tweets/tweet_list.html',
+                  {'tweets': tweets, 'sentiment_data': sentiment_data, 'total_tweets': total_tweets})
+
+
+def export_tweets(request):
+    sentiment = request.GET.get('sentiment')
+    min_confidence = request.GET.get('min_confidence')
+    keyword = request.GET.get('keyword')
+
+    tweets = TweetAnalysis.objects.all()
+
+    if sentiment:
+        tweets = tweets.filter(sentiment=sentiment)
+
+    if min_confidence:
+        try:
+            min_confidence = float(min_confidence)
+            tweets = tweets.filter(confidence__gte=min_confidence)
+        except ValueError:
+            pass  # Ignore invalid input
+
+    if keyword:
+        tweets = tweets.filter(tweet__icontains=keyword)
+
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="tweets.csv"'
+
+    writer = csv.writer(response)
+    writer.writerow(['id', 'tweet', 'sentiment', 'confidence'])
+
+    for tweet in tweets:
+        writer.writerow([tweet.id, tweet.tweet, tweet.sentiment, tweet.confidence])
+
+    return response
+
 
 def graphs(request):
     sentiment_data = get_sentiment_data()
